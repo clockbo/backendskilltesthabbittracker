81f567b9276ab8b080665db8f6f81a47a2427900
baba <sagar.ktechgmail.com>
2023-05-12 06:36:56 +0530

    add google auth

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -180,7 +180,7 @@ const sendResetPasswordMail = async (email, token) => {
       from: "sagar.ktech@gmail.com",
       to: email,
       subject: "for reset password",
-      html: '<p>hi, please click here to <a href="http://127.0.0.1:9999/forget-password?token='+token+'">reset </a>your password',
+      html: '<p>hi, please click here to <a href="https://habbittracker123.up.railway.app/forget-password?token='+token+'">reset </a>your password',
     };
     transporter.sendMail(mailOptions, function (err, info) {
       if (err) {
